91127894eba9e48b8058cd00c317943e310aa24d
Emmanuel Lepage Vallee <elv1313@gmail.com>
2016-10-21 03:40:38 -0400

    objectnode: Support connecting the property
    Something very important was missing, and part of it still is.

--- src/qobjectnode.cpp
+++ src/qobjectnode.cpp
@@ -2,18 +2,44 @@
 #include <QMetaProperty>
 #include <QWidget>
 
-QObjectnode::
-QObjectnode(QObject *data, QGraphicsItem *parent) : GraphicsNode(parent)
+#include "graphicsnodesocket.hpp"
+
+class QObjectnodePrivate : public QObject
 {
-	m_data=data;
-	if(m_data==0)
+public:
+	QObject* _data;
+	QHash<int, QMetaObject::Connection> _source_connections;
+	QHash<int, QMetaObject::Connection> _sink_connections;
+
+public Q_SLOTS:
+	void slotConnected(GraphicsNodeSocket* other);
+};
+
+
+PropertyConnection::PropertyConnection(QObject* parent) : QObject(parent)
+{
+
+}
+
+PropertyConnection::~PropertyConnection()
+{
+	QObject::disconnect(_conn);
+}
+
+QObjectnode::
+QObjectnode(QObject *data, QGraphicsItem *parent) : GraphicsNode(parent),
+	d_ptr(new QObjectnodePrivate())
+{
+	d_ptr->_data = data;
+
+	if(data==0)
 		qWarning("NULL Data Object!");
 	else {
 		QWidget* tst = dynamic_cast<QWidget*>(data);
 		if(tst!=0)
 			setCentralWidget(tst);
 
-		const QMetaObject* m = m_data->metaObject();
+		const QMetaObject* m = data->metaObject();
 		int property_count = m->propertyCount()-1;
 
 		for(;property_count>=0;property_count--) {
@@ -22,10 +48,55 @@ QObjectnode(QObject *data, QGraphicsItem *parent) : GraphicsNode(parent)
 				continue;
 			if(prop.isReadable() && prop.hasNotifySignal())
 			{
-				add_source(QString(prop.name()) + "[" +QString(prop.typeName())  +"]",m_data,property_count);
+				GraphicsNodeSocket* node = add_source(QString(prop.name()) + "[" +QString(prop.typeName())  +"]",data,property_count);
+
+                                PropertyConnection* conn = new PropertyConnection(data);
+                                conn->_prop_id  = property_count;
+                                conn->_source_mo = m;
+                                conn->d_ptr = d_ptr;
+                                node->setProperty("socketData", QVariant::fromValue(conn));
+
 			}
-			if(prop.isWritable())
-				add_sink(QString(prop.name()) + "[" +QString(prop.typeName())  +"]",m_data,property_count);
+			if(prop.isWritable()) {
+				GraphicsNodeSocket* node = add_sink(QString(prop.name()) + "[" +QString(prop.typeName())  +"]",data,property_count);
+
+                                PropertyConnection* conn = new PropertyConnection(data);
+                                conn->_prop_id  = property_count;
+                                conn->_source_mo = m;
+                                conn->d_ptr = d_ptr;
+                                node->setProperty("socketData", QVariant::fromValue(conn));
+
+                                QObject::connect(node, &GraphicsNodeSocket::connectedTo, d_ptr, &QObjectnodePrivate::slotConnected);
+                        }
 		}
 	}
 }
+
+
+void QObjectnodePrivate::
+slotConnected(GraphicsNodeSocket* source_node)
+{
+	const GraphicsNodeSocket* sink_node = qobject_cast<GraphicsNodeSocket*>(QObject::sender());
+
+	if (sink_node && source_node) {
+		const PropertyConnection* srcMO  = qvariant_cast<PropertyConnection*>(source_node->property("socketData"));
+		const PropertyConnection* sinkMO = qvariant_cast<PropertyConnection*>(sink_node->property("socketData"));
+
+		// Connect 2 QObjectnode
+		if (srcMO && sinkMO) {
+		const QMetaProperty sinkProp = sinkMO->_source_mo->property(sinkMO->_prop_id);
+		const QMetaProperty sourceProp = srcMO->_source_mo->property(srcMO->_prop_id);
+
+		const QByteArray sinkName   = sinkProp.name();
+		const QByteArray sourceName = sourceProp.name();
+
+		// Set the target node property to the source current value
+		sinkMO->d_ptr->_data->setProperty(
+			sinkName,
+			srcMO->d_ptr->_data->property(sourceName)
+		);
+		//TODO connect to the signal
+		}
+	}
+}
+// kate: space-indent off;; indent-width 8; replace-tabs off;
--- src/qobjectnode.hpp
+++ src/qobjectnode.hpp
@@ -3,14 +3,33 @@
 
 #include "graphicsnode.hpp"
 
-class QObjectnode: public GraphicsNode
+class QObjectnodePrivate;
+
+class PropertyConnection final : public QObject //TODO make private
+{
+	Q_OBJECT
+public:
+	explicit PropertyConnection(QObject* parent);
+	~PropertyConnection();
+
+	const QMetaObject* _source_mo;
+	QMetaObject::Connection _conn;
+	int _prop_id;
+	QObjectnodePrivate* d_ptr;
+};
+Q_DECLARE_METATYPE(PropertyConnection*)
+
+
+class QObjectnode : public GraphicsNode
 {
 public:
 	QObjectnode(QObject* data, QGraphicsItem *parent = nullptr);
 
 private:
-	QObject* m_data;
+	QObjectnodePrivate* d_ptr;
+	Q_DECLARE_PRIVATE(QObjectnode)
 };
 
 #endif /* __QOBJECTNODE_HPP__FF345507_3840_47A5_BE60_D24FFF0BEE7F */
 
+// kate: space-indent off;; indent-width 8; replace-tabs off;
